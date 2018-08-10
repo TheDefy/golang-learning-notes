aedcb2cfbd597666677938c6c491f188ca700691
chenglei <chenglei@bbtree.com>
2018-08-10 15:52:05 +0800

    接口

--- interfacetest/main.go
+++ interfacetest/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "thedefy/LearningNotes/interfacetest/user"
+import (
+	"thedefy/LearningNotes/interfacetest/user"
+	"fmt"
+	real2 "thedefy/LearningNotes/interfacetest/real"
+	"time"
+)
 
 type Notifier interface {
 	Notify()
+}
+
+type Retriever interface {
+	Get(url string) string
 }
 
 func main() {
@@ -12,10 +21,24 @@ func main() {
 		Name:  "ChengLei",
 		Email: "TheDefy@163.com",
 	}
-
 	sendEmail(&u)
+
+	fmt.Println()
+
+	var r Retriever
+	r = &real2.Retriever{
+		UserAgent: "",
+		TimeOut:   time.Minute,
+	}
+
+	downloads := doDownloads(r)
+	fmt.Println(downloads)
 }
 
 func sendEmail(n Notifier) {
 	n.Notify()
+}
+
+func doDownloads(r Retriever) string {
+	return r.Get("https://github.com/TheDefy/golang-learning-notes")
 }
